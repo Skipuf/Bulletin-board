7e593761781e8b2bae742bb8129295b64b53aaf9
Skipuf <skipuf.ru@gmail.com>
2024-08-26 14:27:52 +0500

    build 1.1

--- Bulletin_board/settings.py
+++ Bulletin_board/settings.py
@@ -101,13 +101,9 @@ WSGI_APPLICATION = 'Bulletin_board.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'postgres',
-        'USER': 'postgres',
-        'PASSWORD': '975481',
-        'HOST': 'localhost',
-        'PORT': '5432',
-    },
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
 
 # Password validation
